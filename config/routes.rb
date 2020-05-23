9096f73a4eb11c9a0bcd38bb931c05d32d91db83
riho0025 <yuxxx26xx@gmail.com>
2020-05-24 00:14:29 +0900

    商品購入確認ページマークアップ

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:show]
   resources :items, only: [:index, :new, :show]
+    resources :purchases, only: [:index]
   resources :profiles, only: [:new, :show]
   resources :addresses, only: [:new]
   
