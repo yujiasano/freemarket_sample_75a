c46dd029765c7503e3c904b50d695999f30f64fd
sinjifuruta <64063851+sinjifuruta@users.noreply.github.com>
2020-05-25 10:27:33 +0900

    commit10

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,8 @@ class ApplicationController < ActionController::Base
     Rails.env.production?
   end
 
+  
+
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
