34b45e1caf8af3e431638fe66d057061dfe4e93f
sinjifuruta <64063851+sinjifuruta@users.noreply.github.com>
2020-05-25 10:19:09 +0900

    mypage1

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:show]
   resources :items, only: [:index, :new, :show]
-  resources :profiles, only: [:new, :show]
+  resources :profiles, only: [:index, :new, :show]
   
 end
