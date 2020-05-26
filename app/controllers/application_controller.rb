8dd9a7e0fc19d3ee63cde093544f3a686f88447a
yujiasano <forever2011.0903@gmail.com>
2020-05-26 10:17:28 +0900

    新規登録ページ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
+  protected
 
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+  
   private
   def production?
     Rails.env.production?
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,29 @@
 Rails.application.routes.draw do
 
   
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+  devise_scope :user do
+    get 'addresses', to: 'users/registrations#new_address'
+    post 'addresses', to: 'users/registrations#create_address'
+  end
+
+
+
+
+
+
+
 
   root 'items#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :home, only: [:index]
   resources :users, only: [:show]
-  resources :items, only: [:index, :new, :show]
+  resources :items do
     resources :purchases, only: [:index]
-  resources :profiles, only: [:new, :show]
+  end
+  resources :profiles, only: [:new, :show, :create]
   resources :addresses, only: [:new]
   
 end
