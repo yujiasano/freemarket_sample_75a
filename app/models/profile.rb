fca1533a02297624b7b036c1e73ab832360d45f2
yujiasano <forever2011.0903@gmail.com>
2020-06-01 10:38:53 +0900

    テストのgemインストール

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,4 @@
 class Profile < ApplicationRecord
   belongs_to :user, optional: true
+  validates :family_name, :first_name, :family_name_kana, :first_name_kana, :phone_number, :birthday, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
+  validates :nickname, presence: true
+  validates :email, presence: true,  uniqueness: true
   has_many :items
   has_one :profile, dependent: :destroy
   has_one :address, dependent: :destroy
