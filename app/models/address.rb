24b5724cdd0c6ab0d8efdf3dd158bfefcb3b48c0
yujiasano <forever2011.0903@gmail.com>
2020-06-03 20:53:44 +0900

    バリデーション修正、エラー時のビュー修正

--- app/models/address.rb
+++ app/models/address.rb
@@ -14,7 +14,7 @@ class Address < ApplicationRecord
   }
   validates :prefecture, :municipalities, :address, presence: true
   validates :postal_code, format: {with: /\A\d{3}[-]\d{4}$|^\d{3}[-]\d{2}$|^\d{3}$|^\d{5}$|^\d{7}\z/}
-  # validates :postal_code, numericality: { only_integer: true }
+  
 
   belongs_to :user, optional: true
 end
